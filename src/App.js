d7c108bbd443093037ea9da1024d213b2d6c3179
Deepak Gupta <deepakgupta60k@gmail.com>
2024-10-29 20:16:11 +0530

    working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import WeatherApp from './WeatherApp';
 
 function App() {
+
+
   return (
     <div className="App">
-      
+      <WeatherApp/>
     </div>
   );
 }
